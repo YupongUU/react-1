2f989e049ebfc3923ce78f009904bc6549b0d920
YupoonUU <fengxiaohai16@163.com>
2017-07-23 16:25:28 +0800

    react*10

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+function tick() {
+  const ele = (
+    <div>
+      <h1>hello2</h1>
+      <h2>
+        it is {new Date().toLocaleDateString()}
+      </h2>
+    </div>
+  );
+
+  ReactDOM.render(ele, document.getElementById("root"));
+  registerServiceWorker();
+}
+
+setInterval(tick(), 1000);
