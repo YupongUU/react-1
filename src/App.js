74453a65b694596485f7e312972729c144b1c2a9
YupoonUU <fengxiaohai16@163.com>
2017-07-23 15:53:44 +0800

    react*5

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ class App extends Component {
       <div className="App">
         <h1>React 运行成功啦</h1>
         <h2>第二次测试React能否在git展示</h2>
+        <h3>disanci cheshi </h3>
       </div>
     );
   }
